fb520ed6db6f60bb863f8682ea19737a163a41d9
Josh McHugh <joshua.mchugh01@gmail.com>
2023-07-06 00:56:17 -0400

    Created bagel buying page

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,50 @@
+import './index.css';
+
+const bagels = [
+    {
+        name: "Plain Bagel",
+        price: "1.60",
+        imgUrl: "/plain.jpg"
+    },
+    {
+        name: "Sesame Bagel",
+        price: "1.60",
+        imgUrl: "/sesame.jpg"
+    },
+    {
+        name: "Cinnamon Raisin Bagel",
+        price: "1.60",
+        imgUrl: "/cinnamon-raisin.jpg"
+    },
+    {
+        name: "Poppy Bagel",
+        price: "1.60",
+        imgUrl: "/poppy.jpg"
+    }
+];
+
 function App() {
-  return (
-    <>
-      Hello World
-    </>
-  );
+    return (
+        <>
+          <main className="container">
+            <h1>Delicous bagels before your eyes</h1>
+            <article className="items">
+              {
+                  bagels.map((bagel, index) => (
+                      <section key={index} className="item">
+                        <img className="item__image" src={bagel.imgUrl}/>
+                        <div className="item__details">
+                          <p className="item__name">{bagel.name}</p>
+                          <p className="item__price">${bagel.price}</p>
+                          <button className="item__btn">Buy</button>
+                        </div>
+                      </section>
+                  ))
+              }
+            </article>
+          </main>
+        </>
+    );
 }
 
 export default App;
