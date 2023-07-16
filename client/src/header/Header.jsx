aef5198f4beeda130fda68a7a237038e05a7bd82
Josh McHugh <joshua.mchugh01@gmail.com>
2023-07-16 11:33:59 -0400

    Navigation header as current branding and home link

--- client/src/header/Header.jsx
+++ client/src/header/Header.jsx
@@ -1,9 +1,12 @@
+import { Link } from 'react-router-dom';
 import Bag from './Bag';
 
 function Header() {
     return (
         <header className="container header">
-          <h1>Delicious Bagels</h1>
+          <Link className="header__brand" to="/">
+            <h1>Delicious Bagels</h1>
+          </Link>
           <Bag />
         </header>
     );
