34d093568e9627e27a9f381c7b4b2606e64c9023
Josh McHugh <joshua.mchugh01@gmail.com>
2023-07-19 01:01:16 -0400

    Cleaned up CheckoutComplete page styles and refactored some code logic

--- client/src/CheckoutComplete.jsx
+++ client/src/CheckoutComplete.jsx
@@ -1,8 +1,29 @@
 import { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { loadStripe } from '@stripe/stripe-js';
 import { Elements, useElements, useStripe } from '@stripe/react-stripe-js';
 
 const stripePromise = loadStripe(import.meta.env.VITE_STRIPE_PUBLIC_KEY);
+
+const MESSAGES = {
+    succeeded: {
+        header: "Payment Successful!",
+        subheader: "Thank you for choosing Delicious Bagels."
+    },
+    processing: {
+        header: "Payment is processing...",
+        subheader: "We are working on processing your payment... hold tight."
+    },
+    requires_payment_method: {
+        header: "Your payment was not successful.",
+        subheader: "Return to check to try again."
+    }
+};
+
+const UNKNOWN_ERROR_MESSAGE = {
+    header: "Something went wrong.",
+    subheader: "Please try again. A notification has been created of this issue."
+};
 
 function CheckoutComplete(props) {
     return (
@@ -17,35 +38,29 @@ function Message(props) {
     const stripe = useStripe();
     const elements = useElements();
 
-    const [ message, setMessage ] = useState(null);
+    const [ messages, setMessages ] = useState({});
 
     useEffect(() => {
         const clientSecret = new URLSearchParams(window.location.search).get("payment_intent_client_secret");
         if(stripe && clientSecret) {
             stripe.retrievePaymentIntent(clientSecret)
-                .then(handlePaymentIntentStatus);
+                .then(({ paymentIntent }) => setMessages(handlePaymentIntentStatus(paymentIntent)));
         }
     }, [stripe]);
 
-    const handlePaymentIntentStatus = ({paymentIntent}) => {
-        switch(paymentIntent.status) {
-        case "succeeded":
-            setMessage("Payment succeeded!");
-            break;
-        case "processing":
-            setMessage("Your payment is processing.");
-            break;
-        case "requires_payment_method":
-            setMessage("Your payment was not successful, please try again.");
-            break;
-        default:
-            setMessage("Something went wrong.");
-            break;
-        }
+    const handlePaymentIntentStatus = (paymentIntent) => {
+        const message = MESSAGES[`${paymentIntent.status}`];
+        return message ? message : UNKNOWN_ERROR_MESSAGE;
     };
 
     return (
-        <div>{message}</div>
+        <article className="checkout-complete">
+          <h5 className="checkout-complete__header">{messages.header}</h5>
+          <p className="checkout-complete__subheader">{messages.subheader}</p>
+          <Link className="checkout-complete__return" to="/">
+            Back to product page
+          </Link>
+        </article>
     );
 }
 
